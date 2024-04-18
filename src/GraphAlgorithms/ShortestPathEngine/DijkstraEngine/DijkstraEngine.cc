9a6b3cf5aafc8215f65740805b41f1cfe1523d77
Evangelina Marj <evangelm@ev-i4.kzn.21-school.ru>
2024-04-18 17:46:44 +0300

    develop-evanglem. Reimplemented Dijkstra algorithm

--- src/GraphAlgorithms/ShortestPathEngine/DijkstraEngine/DijkstraEngine.cc
+++ src/GraphAlgorithms/ShortestPathEngine/DijkstraEngine/DijkstraEngine.cc
@@ -1,22 +1,64 @@
 #include "DijkstraEngine.h"
 
-#include "containers/adaptors/Queue/Queue.h"
 #include <limits>
+#include <unordered_set>
 #include <vector>
+
+#include "common/constants.h"
+#include "containers/adaptors/Queue/Queue.h"
 
 namespace s21 {
 
-int DijkstraEngine::GetShortestPathBetweenVertices(const Graph &graph, int vertex_1, int vertex_2) {
+int DijkstraEngine::GetCurrentMinimalIndex(const std::vector<int> distances, const std::unordered_set<int> visited) const {
+    int min_distance = constants::kInfinity;
+    int min_index = -1;
+    for (int i = 0; i < distances.size(); ++i) {
+        if (visited.find(i) == visited.end() && distances[i] < min_distance) {
+            min_distance = distances[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+void DijkstraEngine::UpdateDistances(const Graph& graph, const int current_vertex, const std::vector<int>& current_distances, std::vector<int>& updated_distances,
+                     const std::unordered_set<int>& visited) {
+    for (int i = 0; i < graph.GetSize(); ++i) {
+        const int current_distance = graph.GetDistance(current_vertex, i);
+        if (current_distance != constants::kInfinity && visited.find(i) == visited.end()) {
+            if (updated_distances[i] > current_distances[current_vertex] + current_distance) {
+                updated_distances[i] = current_distances[current_vertex] + graph.GetDistance(current_vertex, i);
+            }
+        }
+    }
+}
+
+int DijkstraEngine::GetShortestPathBetweenVertices(const Graph& graph, int vertex_1, int vertex_2) {
     vertex_1 -= 1;
     vertex_2 -= 1;
-    if (vertex_1 < 0 || vertex_2 < 0 || vertex_1 >= graph.GetGraph().size() || vertex_2 >= graph.GetGraph().size()) {
+    if (vertex_1 < 0 || vertex_2 < 0 || vertex_1 >= graph.GetGraph().size() || vertex_2 >= graph.GetSize()) {
         return 0;
     }
 
-    std::vector<std::vector<int>> adjacency_matrix = graph.GetGraph();
+    std::vector<int> current_distances(graph.GetSize(), constants::kInfinity);
+    std::vector<int> updated_distances(graph.GetSize());
+    std::unordered_set<int> visited{};
 
+    int current_vertex = vertex_1;
 
+    current_distances[current_vertex] = 0;
 
-    return kInfinity;
+    while (visited.size() < graph.GetSize() && current_vertex != -1) {
+
+        visited.insert(current_vertex);
+        updated_distances = current_distances;
+
+        UpdateDistances(graph, current_vertex, current_distances, updated_distances, visited);
+        current_vertex = GetCurrentMinimalIndex(updated_distances, visited);
+
+        current_distances = updated_distances;
+    }
+
+    return updated_distances[vertex_2];
 }
 }// namespace s21
--- tests/s21_graph_algorithms_shortest_path_engine.cc
+++ tests/s21_graph_algorithms_shortest_path_engine.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "GraphAlgorithms/GraphAlgorithms.h"
+#include "common/constants.h"
 #include "constants.h"
 
 namespace s21 {
@@ -13,12 +14,115 @@ class GraphAlgorithmsShortestPathEngineTest : public ::testing::Test {
 
 TEST_F(GraphAlgorithmsShortestPathEngineTest, Test0) {
     graph_.LoadGraphFromFile(constants::kFileNames[0]);
-    int shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, 1, 10);
-    std::cerr << "shortest_path: " << shortest_path << "\n";
+    // int shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, 1, 10);
+    // std::cerr << "shortest_path: " << shortest_path << "\n";
 }
 TEST_F(GraphAlgorithmsShortestPathEngineTest, Test1) {
-    graph_.LoadGraphFromFile(constants::kFileNames[4]);
-    const int shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, 1, 2);
-    std::cerr << "shortest_path: " << shortest_path << "\n";
+    graph_.LoadGraphFromFile(constants::kFileNames[5]);
+    int vertex_1 = 0;
+    int vertex_2 = 0;
+    int shortest_path = 0;
+
+    vertex_1 = 1;
+    vertex_2 = 6;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 9);
+
+    vertex_1 = 1;
+    vertex_2 = 5;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 6);
+
+    vertex_1 = 1;
+    vertex_2 = 4;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 8);
+
+    vertex_1 = 1;
+    vertex_2 = 3;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 3);
+
+    vertex_1 = 1;
+    vertex_2 = 2;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 2);
+
+    vertex_1 = 1;
+    vertex_2 = 1;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 0);
+}
+TEST_F(GraphAlgorithmsShortestPathEngineTest, Test2) {
+    graph_.LoadGraphFromFile(constants::kFileNames[6]);
+    int vertex_1 = 0;
+    int vertex_2 = 0;
+    int shortest_path = 0;
+
+    vertex_1 = 1;
+    vertex_2 = 6;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, constants::kInfinity);
+
+    vertex_1 = 1;
+    vertex_2 = 5;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 25);
+
+    vertex_1 = 1;
+    vertex_2 = 4;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 10);
+
+    vertex_1 = 1;
+    vertex_2 = 3;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 45);
+
+    vertex_1 = 1;
+    vertex_2 = 2;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 45);
+
+    vertex_1 = 1;
+    vertex_2 = 1;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 0);
+}
+TEST_F(GraphAlgorithmsShortestPathEngineTest, Test3) {
+    graph_.LoadGraphFromFile(constants::kFileNames[6]);
+    int vertex_1 = 0;
+    int vertex_2 = 0;
+    int shortest_path = 0;
+
+    vertex_1 = 6;
+    vertex_2 = 1;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 48);
+
+    vertex_1 = 6;
+    vertex_2 = 2;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 23);
+
+    vertex_1 = 6;
+    vertex_2 = 3;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 33);
+
+    vertex_1 = 6;
+    vertex_2 = 4;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 38);
+
+    vertex_1 = 6;
+    vertex_2 = 5;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 3);
+
+    vertex_1 = 6;
+    vertex_2 = 6;
+    shortest_path = graph_algorithms_.GetShortestPathBetweenVertices(graph_, vertex_1, vertex_2);
+    ASSERT_EQ(shortest_path, 0);
 }
 }// namespace s21
