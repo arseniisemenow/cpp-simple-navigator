3ae402729380108ef4f03dcf7a08e5eab7ffd44e
theseChains <rasim0103@gmail.com>
2024-08-12 15:53:24 +0300

    develop-dusksher-mint. Fix filesystem includes

--- src/cli/ChooseFilePath/FilePathHandler.h
+++ src/cli/ChooseFilePath/FilePathHandler.h
@@ -2,6 +2,7 @@
 #define A2_SIMPLENAVIGATOR_V1_0_CPP_1_SRC_CLI_CHOOSEFILEPATH_CHOOSEFILEPATH_H_
 
 #include <string>
+#include <vector>
 
 std::vector<std::string> ListDirectories(const std::string &path);
 void ChooseFilePath(std::string &dataset_path);
